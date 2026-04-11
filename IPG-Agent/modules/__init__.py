2fc3e7d4836c00c74930fc8ccc256b3397cba0f6
mrpk7431-dev <mrpk7431@gmail.com>
2026-04-11 06:18:08 +0530

    Fix: Updated modules/__init__.py to remove Selenium dependency

--- IPG-Agent/modules/__init__.py
+++ IPG-Agent/modules/__init__.py
@@ -1,17 +1,13 @@
-from .linkedin import LinkedInBot
+from .linkedin_api import LinkedInAPI
 from .ai_content import AIContentGenerator
 from .hashtag_engine import HashtagEngine
-from .job_searcher import JobSearcher
-from .whatsapp import WhatsAppNotifier
 from .resume_parser import ResumeParser
 from .scheduler import TaskScheduler
 
 __all__ = [
-    'LinkedInBot',
+    'LinkedInAPI',
     'AIContentGenerator',
     'HashtagEngine',
-    'JobSearcher',
-    'WhatsAppNotifier',
     'ResumeParser',
     'TaskScheduler',
 ]
